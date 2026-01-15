7b6b8c94a8594929c0d105d822f6acff43988853
suj5453-star <suj5453@gmail.com>
2026-01-15 09:57:39 +0900

    Create Day25.cpp

---
+++ CodeKata_Day15~/CodeKata_Day15~/Day25.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int absolutes[], size_t absolutes_len, bool signs[], size_t signs_len) {
+    int answer = 0;
+
+    for (int i = 0; i < signs_len; i++) {
+        if (signs[i] == false) {
+            answer -= absolutes[i];
+        }
+        else {
+            answer += absolutes[i];
+        }
+    }
+    return answer;
+}
