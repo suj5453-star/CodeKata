156b518c93c2e722e5c1f459185c53834c2e44b2
suj5453-star <suj5453@gmail.com>
2026-01-28 09:37:56 +0900

    Create Day31.cpp

---
+++ CodeKata_Day15~/CodeKata_Day15~/Day31.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int a[], size_t a_len, int b[], size_t b_len) {
+    int answer = 0;
+
+    if (a_len == b_len)
+    {
+        for (int i = 0; i < a_len; i++)
+        {
+            answer += a[i] * b[i];
+        }
+    }
+    return answer;
+}
