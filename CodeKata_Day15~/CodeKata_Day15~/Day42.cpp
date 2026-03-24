ee1ffa5f2b700e5297e0b6be4c796fdaa1930421
SEOWOOJEONG <unha1@naver.com>
2026-03-24 10:11:06 +0900

    Day42

--- CodeKata_Day15~/CodeKata_Day15~/Day42.cpp
+++ CodeKata_Day15~/CodeKata_Day15~/Day42.cpp
@@ -5,24 +5,24 @@ using namespace std;
 
 int solution(vector<int> number) {
     int answer = 0;
-    
+
     // 양 0 음 / 번호 중복 가능 / 길이 제한 있음
-    for(int i = 0; i < number.size(); i++)
+    for (int i = 0; i < number.size(); i++)
     {
         int sum = 0;
         sum += number[i];
-        
-        for(int j = i + 1; j < number.size(); j++)
+
+        for (int j = i + 1; j < number.size(); j++)
         {
             sum = number[i] + number[j];
-            
-            for(int k = j + 1; k < number.size(); k++)
+
+            for (int k = j + 1; k < number.size(); k++)
             {
                 sum = number[i] + number[j] + number[k];
                 if (sum == 0) { answer++; }
             }
         }
     }
-    
+
     return answer;
 }
